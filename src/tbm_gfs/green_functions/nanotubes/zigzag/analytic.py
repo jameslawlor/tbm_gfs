4d7bb1da7137f0b7862b1e3fa450388c4e07c75e
James Lawlor <jameslawlor1987@gmail.com>
2024-01-07 13:30:14 +0200

    zznt integral and analytic working, tests added

--- src/tbm_gfs/green_functions/nanotubes/zigzag/analytic.py
+++ src/tbm_gfs/green_functions/nanotubes/zigzag/analytic.py
@@ -1,32 +1,48 @@
-# def analytic(En, Nc, m, n, S1, S2):
-#     if (m + n) < 0:
-#         S1, S2 = S2, S1
-#         m, n = -m, -n
-#     G = 0
-#     for j in range(1, Nc + 1):
-#         kZ = j * pi / Nc
-#         q = acos((En**2 - t**2 - 4 * (t**2) * (cos(kZ) ** 2)) / (4 * t * t * cos(kZ)))
-#         if q.imag < 0:
-#             q = -q
+from cmath import pi, cos, sin, acos, exp
 
-#         if (j == Nc / 2.0) and (m + n == 0):
-#             if S1 == S2:
-#                 Ne = En
-#             else:
-#                 Ne = t
-#             G += (Ne * exp(i * kZ * (m - n))) / (Nc * (En**2 - t**2))
-#         else:
-#             if S1 == S2:
-#                 Ne = En
-#             if S1 < S2:
-#                 Ne = t + 2 * t * cos(kZ) * exp(+i * q)
-#             if S1 > S2:
-#                 if m == 0 and n == 0:
-#                     Ne = t + 2 * t * cos(kZ) * exp(+i * q)
-#                 else:
-#                     Ne = t + 2 * t * cos(kZ) * exp(-i * q)
-#             G += (i / (4 * Nc * (t * t))) * (
-#                 (Ne * exp(i * 2 * kZ * (m - n)) * exp(i * q * (m + n)))
-#                 / (cos(kZ) * sin(q))
-#             )
-#     return G
+from tbm_gfs.green_functions.functions import (
+    Ne_lambda_function,
+    phase_lambda_function,
+)
+from tbm_gfs.constants import ETA, FIRST_NEAREST_NEIGHBOUR_HOPPING_ENERGY
+
+t0 = FIRST_NEAREST_NEIGHBOUR_HOPPING_ENERGY
+
+def green_function(n_c, E, m, n, S1, S2):
+    E += 1j*ETA
+
+    if (m + n) < 0:
+        S1, S2 = S2, S1
+        m, n = -m, -n
+    G = 0
+
+    for j in range(1, n_c + 1):
+        kZ = j * pi / n_c
+
+        q = acos((E**2 - t0**2 - 4 * (t0**2) * (cos(kZ) ** 2)) / (4 * t0 * t0 * cos(kZ)))
+        if q.imag < 0:
+            q = -q
+
+        if (j == n_c / 2.0) and (m + n == 0):
+            if S1 == S2:
+                Ne = E
+            else:
+                Ne = t0
+            G += (Ne * exp(1j * kZ * (m - n))) / (n_c * (E**2 - t0**2))
+
+        else:
+            if S1 == S2:
+                Ne = E
+            if S1 < S2:
+                Ne = t0 + 2 * t0 * cos(kZ) * exp(1j * q)
+            if S1 > S2:
+                if m == 0 and n == 0:
+                    Ne = t0 + 2 * t0 * cos(kZ) * exp(1j * q)
+                else:
+                    Ne = t0 + 2 * t0 * cos(kZ) * exp(-1j * q)
+
+            G += (1j / (4 * n_c * (t0 * t0))) * (
+                (Ne * exp(1j * kZ * (m - n)) * exp(1j * q * (m + n)))
+                / (cos(kZ) * sin(q))
+            )
+    return G
---
+++ tests/test_zigzag_nanotube_gf_equalities.py
@@ -0,0 +1,79 @@
+import pytest
+import numpy as np
+from tbm_gfs.green_functions.nanotubes.zigzag.analytic import (
+    green_function as zigzag_analytic_gf,
+)
+from tbm_gfs.green_functions.nanotubes.zigzag.integral import (
+    green_function as zigzag_integral_gf,
+)
+from tbm_gfs.constants import (
+    FIRST_NEAREST_NEIGHBOUR_HOPPING_ENERGY,
+)
+
+
+t0_abs = np.abs(FIRST_NEAREST_NEIGHBOUR_HOPPING_ENERGY)
+
+
+@pytest.mark.parametrize(
+    "input_energy", np.linspace(start=-3.0 * t0_abs, stop=3.0 * t0_abs, num=13)
+)
+@pytest.mark.parametrize(
+    "nanotube_cirumferences",
+    [
+        6,
+        11,
+    ],
+)
+@pytest.mark.parametrize(
+    "m_and_n_vectors",
+    [
+        (0, 0), 
+        (1, 1), 
+        (1, 0), 
+        (8, 3),
+     ],
+)
+@pytest.mark.parametrize(
+    "s1_and_s2_sublattices",
+    [
+        (1, 1),
+        (1, 2),
+        (2, 1),
+    ],
+)
+def test_zigzag_nanotube_analytic_vs_integral_gf_similarity(
+    input_energy,
+    nanotube_cirumferences,
+    m_and_n_vectors,
+    s1_and_s2_sublattices,
+    max_relative_integration_comparison_error,
+):
+    (m, n) = m_and_n_vectors
+    (s1, s2) = s1_and_s2_sublattices
+    n_c = nanotube_cirumferences
+    zigzag_analytic_result = zigzag_analytic_gf(
+        n_c,
+        input_energy,
+        m,
+        n,
+        s1,
+        s2,
+    )
+    zigzag_integral_result = zigzag_integral_gf(
+        n_c,
+        input_energy,
+        m,
+        n,
+        s1,
+        s2,
+    )
+    assert zigzag_analytic_result.real == pytest.approx(
+        zigzag_integral_result.real,
+        rel=max_relative_integration_comparison_error,
+        abs=max_relative_integration_comparison_error,
+    )
+    assert zigzag_analytic_result.imag == pytest.approx(
+        zigzag_integral_result.imag,
+        rel=max_relative_integration_comparison_error,
+        abs=max_relative_integration_comparison_error,
+    )
