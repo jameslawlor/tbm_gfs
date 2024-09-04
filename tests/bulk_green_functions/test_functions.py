7d98e2d9ee1eea19b99b0cbc2bc256498af3e867
James Lawlor <jameslawlor1987@gmail.com>
2024-09-04 20:43:22 +0300

    not working, save state

--- tests/bulk_green_functions/test_functions.py
+++ tests/bulk_green_functions/test_functions.py
@@ -7,7 +7,7 @@ from tbm_gfs.bulk_green_functions.functions import (
 @pytest.mark.parametrize("m, n, s1, s2, expected", [
     (2, 1, 1, 1, (2, 1, 1, 1)),
     (5, 1, 1, 2, (5, 1, 1, 2)),
-    (5, 5, 2, 2, (5, 5, 1, 1)),
+    (5, 5, 2, 2, (5, 5, 2, 2)),
     (0, 0, 2, 1, (0, 0, 1, 2)), # w-b case
 ])
 def test_sector_0(m, n, s1, s2, expected):
@@ -15,10 +15,11 @@ def test_sector_0(m, n, s1, s2, expected):
 
 
 @pytest.mark.parametrize("m, n, s1, s2, expected", [
-    (1, 1, 1, 2, (1, 1, 2, 1)),
+    (1, -1, 1, 2, (1, 1, 2, 1)),
 ]) 
 def test_sector_1(m, n, s1, s2, expected):
-    assert graphene_sector_method(m, n, s1, s2) == expected
+    result = graphene_sector_method(m, n, s1, s2)
+    assert result == expected
 
 # def test_sector_2():
 #     # Test for sector 2 where abs(n) > m
